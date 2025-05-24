123b9a8f2a237c585898bd1805173e024ab3af8e
kaizzerr <kaizzergatan@gmail.com>
2025-05-23 20:57:50 -0600

    Error Handling Activities

--- utilities/index.js
+++ utilities/index.js
@@ -24,12 +24,14 @@ Util.getNav = async function (req, res, next) {
   return list
 }
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
 module.exports = Util
-
-// Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-
-module.exports = router;
 
 /* **************************************
 * Build the classification view HTML
