ab98791823b097f89072ad3db605debebd14591f
kaizzerr <kaizzergatan@gmail.com>
2025-05-29 20:15:31 -0600

    Database Connection

--- public/js/password.js
+++ public/js/password.js
@@ -1,6 +1,6 @@
 const showPw = document.querySelector("#showPw");
 showPw.addEventListener("click", function() {
-    const pwInput = document.getElementById("password");
+    const pwInput = document.getElementById("account_password");
     const type = pwInput.getAttribute("type");
     if (type == "password") {
         pwInput.setAttribute("type", "text");
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,7 +7,10 @@ const accountController = require("../controllers/accountController")
 // Login route
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
-// Registration route
-router.get("/register", utilities.handleErrors(accountController.buildRegister))
+// Registration route (GET)
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Registration route (POST)
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router
